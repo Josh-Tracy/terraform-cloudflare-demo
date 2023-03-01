36f99e35dc2dfd0f26f27d744a4847d075c48014
Joshua Tracy <55331456+Josh-Tracy@users.noreply.github.com>
2023-03-01 15:42:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable "cloudflare_zone_id" {
   type        = string
   description = "The Zone ID of the Record to create."
 }
+
+variable "name" {
+  type        = string
+  description = "Name of A record host"
+}
+
+variable "ip" {
+  type        = string
+  description = "IP of a record host"
+}
